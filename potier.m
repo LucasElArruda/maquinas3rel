77e3fec43d73f5b373c957166aa7120fadaaa0e5
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@embedded.ufcg.edu.br>
2018-03-05 17:27:39 -0300

    Adiciona script que aplica o método de potier
    Por enquanto só vai até o passo 4.

---
+++ potier.m
@@ -0,0 +1,28 @@
+% Entradas
+deltaVl = 158.4;    %4.4cm * 36
+Ra = 38.671 ;       % R75 do laboratório 2
+Vt = 380;         % Tensão de regime do ensaio
+fp = 0.8;
+Ia = 800*10^(-3);   % Corrente de regime em ensaio
+
+% Passo 1: Cálculo de Xl
+Xl = deltaVl/(sqrt(3)*Ia);
+
+% Passo 2: Cálculo de Xp
+Xp = 1.20*Xl;
+
+% Passo 3: Determinação da tensão de Potier (Vp)
+Vpfase = Vt/sqrt(3) + Ra*(Ia * (cos(acos(fp)) - j*sin(acos(fp)))) + j*Xp*(Ia * (cos(acos(fp)) - j*sin(acos(fp))));
+
+Vplinha = abs(Vpfase)*sqrt(3);
+
+Vplinhacm = Vplinha/36;   % Depende da escala escolhinda para o gráfico
+
+% Passo 4: Fator de saturação (Ks)
+IfvpCAcm = 11.4;
+IfvpEntreferrocm = 9.7;
+
+IfvpCA = IfvpCAcm*30;   % Depende da escala de if
+IfvpEntreferro = IfvpEntreferrocm*30;
+
+Ks = IfvpCA/IfvpEntreferro;
