6337a2c6d509c07410bff699c33358aa5c4cefe6
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@ee.ufcg.edu.br>
2018-03-06 11:42:41 -0300

    Adiciona passo 3 do método IEEE

--- ieee.m
+++ ieee.m
@@ -9,3 +9,21 @@ Ia = 800*10^(-3);   % Corrente de regime do ensaio
 
 % Passo 1: Calculo de Xl
 Xp = deltaVl/(sqrt(3)*Ia);
+
+% Passo 2: Determinacao da tensao de Potier (Vp)
+Vpfase = Vt/sqrt(3) + Ra*(Ia * (cos(acos(fp)) - j*sin(acos(fp)))) + j*Xp*(Ia * (cos(acos(fp)) - j*sin(acos(fp))));
+
+% Passo 3: Componentes da corrente de campo
+Ifgcm = 4.9;
+Ifsicm = 13.8;
+Ifscm = 8.8;
+
+Ifg = Ifgcm*30;
+Ifsi = Ifsicm*30;
+Ifs = Ifscm*30;
+
+Ifl = Ifs + sqrt((Ifg + Ifsi*sin(acos(fp)))^2 + (Ifsi*cos(acos(fp)))^2);
+
+
+
+% Passo 4
